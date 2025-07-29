a7a64870bf21c047b02ca7965933a149fd86f63c
lowkeshonline <lowkeshonline@gmail.com>
2025-07-29 23:25:02 +0530

    useRef practice

--- react-practice-3/src/App.jsx
+++ react-practice-3/src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // ---- Use Ref for Dom Reference ----
+
+  // const inputRef = useRef(null);
+
+  // function focusInput(){
+  //   inputRef.current.focus();
+  // }
+
+
+  // return (
+  //   <>
+  //     <h2>Sign Up Form</h2>
+  //     <input ref={inputRef} type="text" />
+  //     <input type="text" />
+  //     <button onClick={focusInput}>Submit</button>
+  //   </>
+  // )
+
+  // --- UseRef for reference to value so that i persists across the re renders and also doesn't trigger a re-render just like state does
+  
+  const [count, setCount] = useState(0);
+  const intervalRef = useRef(0);
+
+  function startCounter () {
+    let timer = setInterval(() => {
+      setCount(count => count + 1);
+    }, 1000);
+    intervalRef.current = timer;
+  }
+
+  function stopCounter () {
+    clearInterval(intervalRef.current);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>{count}</h2>
+      <button onClick={startCounter}>Start</button>
+      <button onClick={stopCounter}>Stop</button>
     </>
   )
 }
